2c5e9240b7406265a3ca5a687362b91cd185ff9b
TreyComer <jaacomer@iu.edu>
2020-09-17 12:26:55 -0700

    c

--- Assignment1/assignment1_1.py
+++ Assignment1/assignment1_1.py
@@ -1,34 +1,36 @@
+#Did alone
+
 #INPUT two values
 #RETURN A and B
 def myAnd(A,B):
 #TO DO: Implement function
-
+    return A and B
 
 #INPUT two values
 #RETURN A or B 
 def myOr(A,B):
 #TO DO: Implement function
-
+    return A or B
 
 #INPUT one value
 #RETURN not A
 def myNot(A):
 #TO DO: Implement function
-
+    return not A
 
 #INPUT two values
 #RETURN not (A and B) (called nand)
 #REQUREMENTS use only previous functions
 def myNand(A,B):
 #TO DO: Implement function
-
+    return nand A and B
 
 #INPUT two values
 #RETURN not (A or B) (called nor)
 #REQUIREMENTS use only previous functions
 def myNor(A,B):
 #TO DO: Implement function
-
+    return nor A or B
 
 #INPUT two values A,B
 #Return material implication A => B 
--- Assignment1/assignment1_2.py
+++ Assignment1/assignment1_2.py
@@ -1,3 +1,5 @@
+#Did alone
+
 import math
 import random as rn
 
@@ -7,35 +9,35 @@ import random as rn
 #REQUIREMENTS 
 def myPlus(A,B):
 #TO DO: Implement function
-
+    return A+B
 
 #INPUT two values A,B
 #Return A - B
 #REQUIREMENTS 
 def myMinus(A,B):
 #TO DO: Implement function
-
+    return A-B
 
 #INPUT two values A,B
 #Return A divided by B
 #REQUIREMENTS 
 def myDivide(A,B):
 #TO DO: Implement function
-
+    return A/B
 
 #INPUT two values A,B
 #Return A x B
 #REQUIREMENTS 
 def myProduct(A,B):
 #TO DO: Implement function
-
+    return A*B
 
 #INPUT two values A,B
 #Return A raise to the power of B
 #REQUIREMENTS 
 def myExponent(A,B):
 #TO DO: Implement function
-
+    return A^B
 
 #INPUT two values A,B
 #B is not zero
@@ -43,31 +45,32 @@ def myExponent(A,B):
 #REQUIREMENTS: Use myExponent function
 def myRoot(A,B):
 #TO DO: Implement function
-
+    return Math.Sqrt(A^B)
 
 #INPUT one non-negative value A and base x
 #Return log base x of A
 #REQUIREMENTS use math module
 def myLog(A,x):
 #TO DO: Implement function
-
+    return Math.Log(x, A)
 
 #INPUT one value A
 #Return absolute value of A
 #REQUIREMENTS 
 def myAbs(A):
 #TO DO: Implement function
-
+    return Math.Abs(A)
 
 #INPUT one value A
 #Return e raised to A (Euler's constant)
 #REQUIREMENTS use math module
 def myExp(A):
 #TO DO: Implement function
-
+    return Math.Exp(A)
 
 #INPUT one value A
 #Return floor of A, greatest integer less than A
 #REQUIREMENTS use math module
 def myFloor(A):
 #TO DO: Implement function
+    return Math.Floor(A)
